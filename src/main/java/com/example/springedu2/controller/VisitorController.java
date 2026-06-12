f771643ad41193f2d6bc217eef3af127a9281595
choisiyoung02 <azxx0731@naver.com>
2026-06-12 14:05:35 +0900

    visitorInsert

--- src/main/java/com/example/springedu2/controller/VisitorController.java
+++ src/main/java/com/example/springedu2/controller/VisitorController.java
@@ -25,22 +25,26 @@ public class VisitorController {
 
     //목록 조회
     @GetMapping("/vlist")
-    public String list(Model model) {
+    public ModelAndView list() {
         List<Visitor> visitors = visitorRepository.findAll();
-        model.addAttribute("visitors", visitors);
-        model.addAttribute("msg", null);
-        return "visitorView";
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("visitorView");
+        mv.addObject("visitors", visitors);
+        mv.addObject("msg", null);
+        return mv;
     }
 
     //검색
     @GetMapping("/vsearch")
-    public String search(@RequestParam(defaultValue = "") String key, Model model) {
+    public ModelAndView search(@RequestParam(defaultValue = "") String key) {
         List<Visitor> visitors = key.isBlank()
                 ? visitorRepository.findAll()
                 : visitorRepository.findByMemoContainingIgnoreCaseOrderByIdDesc(key);
-        model.addAttribute("visitors", visitors);
-        model.addAttribute("msg", "메인페이지로 돌아가기");
-        return "visitorView";
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("visitorView");
+        mv.addObject("visitors", visitors);
+        mv.addObject("msg", "메인페이지로 돌아가기");
+        return mv;
     }
 
     //단건 조회
@@ -53,17 +57,29 @@ public class VisitorController {
     }
 
     //등록
+    // @Valid : form 에서 넘어온 데이터를 @Entity 에 있는
+    // 설정(@Id, @NotBlank, @Column(nullable=false) )
+    // 과 비교해서 입력 데이터를 검증
+    // BindingResult : 객체에서 오류가 발생 시 코드를 담아주는 역할
     @PostMapping("/vinsert")
     @Transactional
     public String insert(@Valid Visitor visitor, BindingResult bindingResult, Model model) {
+
+        System.out.println("visitor: " + visitor);
+        System.out.println("BindingResult: " + bindingResult);
+        // 오류 검증
         if (bindingResult.hasErrors()) {
             model.addAttribute("msg", "이름과 내용을 모두 입력하세요. ");
         }
-        visitorRepository.save(visitor);
+        visitorRepository.save(visitor);            // entity 객체를 사용해야함
         return "redirect:/vlist";
     }
 
     //수정
+    // RedirectAttributes : 리디렉션을 수행할 때 한 컨트롤러 메서드에서 다른 컨트롤러
+    // 메서드로 Attributes 를 전달하는데 이용한다. -> 필요할 때 : redirect 를 하여 변경된 값을 넘겨줄 때
+    // addAttribute : 브라우저의 주소창에 보이게 URL에 추가하여 정보 전달
+    // addFlashAttributes : 세션에 저장되고 오직 다음 요청에서만 접근 가능
     @PostMapping("/vupdate")
     @Transactional
     public String update(@Valid Visitor visitor,
--- src/main/java/com/example/springedu2/entity/Visitor.java
+++ src/main/java/com/example/springedu2/entity/Visitor.java
@@ -10,6 +10,7 @@ import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -17,17 +18,26 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @ToString
 public class Visitor {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
 
+    @Id // 기본키 : Primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 자동증가
+    private Integer id; // 방명록 고유식별번호 : 기본키
+
+    // NotBlank : null, ""(빈문자열), " " : 공백포함된 문자열 전부 허용 X
+    // Size(max=50) : 문자열(50문자), 배열(50개), ArrayList(50개)
     @NotBlank(message = "이름은 필수입니다.")
     @Size(max = 50)
     private String name;
 
+    // data 등록일(Created Date) 자동입력 일일이 LocalDateTime.now() 를 넣지 않아도 된다.
     @CreationTimestamp
     @Column(name = "writedate", nullable = false, updatable = false)
-    private LocalDate writedate;
+    private LocalDate writeDate;
+
+    @PrePersist
+    public void prePersist() {
+        writeDate = LocalDate.now();
+    }
 
     @NotBlank(message = "내용은 필수입니다.")
     @Size(max = 1000)
